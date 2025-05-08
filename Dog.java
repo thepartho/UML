47a498b7bd26cdae872dcdfe7e2874aab83b8538
Suvangshu Halder Partho <shpartho05@gmail.com>
2025-05-09 05:35:01 +0600

    Dog.java

--- Dog.java
+++ Dog.java
@@ -1,25 +1,25 @@
 package animal;
-
-
+/**
+ *
+ * @author PARTHO7
+ */
 public class Dog extends Animal implements Pet{
-    private String breed;
-
-   public Dog(String name, int age, String breed) {
-        super(name, age);
-        this.breed = breed;
+  private String breed;
+public Dog(String name, int age, String breed) {
+  this.breed = breed; 
+  super(name, age);
+       
     }
 
     public void displayBreed() {
-        System.out.println("Breed: " + breed);
+    System.out.println("Breed: " + breed);
     }
-
     @Override
     public void makeSound() {
-        System.out.println(name + " says: Woof!");
+     System.out.println(name + " says: Woof!");
     }
-
     @Override
     public void play() {
-        System.out.println(name + " the dog is playing fetch!");
+    System.out.println(name + " the dog is playing fetch!");
     }
 }
