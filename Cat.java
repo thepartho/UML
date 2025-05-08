a20ee821c42acf9522306e2f4223176e2bff1dc8
Suvangshu Halder Partho <shpartho05@gmail.com>
2025-05-09 05:33:43 +0600

    Cat.java

--- Cat.java
+++ Cat.java
@@ -1,24 +1,25 @@
 package animal;
-
-
+/**
+ *
+ * @author PARTHO7
+ */
 public class Cat extends Animal implements Pet{
      private String color;
 
     public Cat(String name, int age, String color) {
-        super(name, age);
-        this.color = color;
+        this.color = color; super(name, age);
+     
     }
     public void displayColor() {
         System.out.println("Color: " + color);
     }
-
     @Override
     public void makeSound() {
         System.out.println(name + " says: Meow!");
     }
-
     @Override
-    public void play() {
-        System.out.println(name + " the cat is playing with a ball of yarn!");
+public void play() {
+System.out.println(name + " the cat is playing with a ball of yarn!");
+   
     }
 }
