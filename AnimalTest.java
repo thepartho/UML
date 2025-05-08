076e8702d5b4cebda117484cfe1d6cfd1b554281
Suvangshu Halder Partho <shpartho05@gmail.com>
2025-05-09 05:36:56 +0600

    AnimalTest.java

--- AnimalTest.java
+++ AnimalTest.java
@@ -6,19 +6,18 @@ package animal;
 
 
 public class AnimalTest {
-    
     public static void main(String[] args) {
         
         Animal[] animals = new Animal[2];
-        animals[0] = new Dog("Buddy", 3, "Golden Retriever");
-        animals[1] = new Cat("Whiskers", 2, "Black");
+        animals[0] = new Dog("Tommy", 3, "Bluish White");
+        animals[1] = new Cat("Pookie", 2, "Black");
 
         for (Animal animal : animals) {
             animal.makeSound();
-            
-            if (animal instanceof Pet) {
+        
+            if (animal instanceof Pet) 
+            {
              ((Pet) animal).play();
-            
             }
        
         }
